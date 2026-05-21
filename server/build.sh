ba125356afc368ab8bce6a7a14ff9df95771ab77
Seyma Kaya <seyma.kaya2@hva.nl>
2026-05-21 13:30:09 +0200

    fix: railway command added manually

--- server/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing accessibility-checker..."
-npm install -g accessibility-checker
-
-echo "Building Spring Boot app..."
-chmod +x mvnw
-./mvnw -DoutputFile=target/mvn-dependency-list.log -B -DskipTests clean dependency:list install
